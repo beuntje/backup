4829913c43c17f2e8f839c9a1e6ffbe722e1da28
Benedikt Beun <benedikt.beun@roularta.be>
2024-03-05 11:24:44 +0100

    initial commit

---
+++ readme.md
@@ -0,0 +1,24 @@
+# Backup installer
+
+This script will mount nas and install backup script
+
+## Install
+To run this script on your device, first make sure you've got git installed:
+```
+sudo apt-get install git
+```
+
+Then download this repo:
+```
+git clone https://github.com/beuntje/backup/setup.sh
+```
+
+Make the installer executable:
+```
+chmod +x backup/setup.sh
+```
+
+Finally, run the installer:
+```
+backup/setup.sh IP USERNAME PASSWORD
+```
---
+++ setup.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+BASEDIR=$(pwd)
+
+if [ "$1" = "" ] || [ "$2" = "" ] || [ "$3" = "" ]; then
+  echo "USAGE: "
+  echo "setup.sh IP USERNAME PASSWORD"
+  exit
+fi
+
+IP=$1
+USERNAME=$2
+PASSWORD=$3
+
+# ADD MOUNT FOLDER
+FOLDER="/mnt/backup"
+FOLDER="/home/benedikt/web/woodwing/temp/test"
+if [ ! -d "$FOLDER" ]; then
+  sudo mkdir "$FOLDER"
+  echo "Folder $FOLDER did not exist. "
+fi
+
+# MOUNT NAS
+FSTABFILE="/etc/fstab"
+FSTABFILE="/home/benedikt/web/woodwing/temp/test/fstab.txt"
+if [ ! -n "$(grep -P "[[:space:]]/mnt/backup/[[:space:]]" $FSTABFILE)" ]; then
+  sudo printf "\n//$IP/backups  /mnt/backup/  cifs  user=$USERNAME,password=$PASSWORD,vers=1.0,_netdev,x-systemd.automount  0  0" | sudo tee -a "$FSTABFILE" >/dev/null
+  echo "Mount added "
+fi
+
