fa10a3179f1d8d15f29aa071f542d102e4f604ee
Benedikt Beun <benedikt.beun@roularta.be>
2024-03-06 10:09:33 +0100

    mount command

--- setup.sh
+++ setup.sh
@@ -22,7 +22,7 @@ FSTABFILE="/etc/fstab"
 if [ ! -n "$(grep -P "[[:space:]]/mnt/backup/[[:space:]]" $FSTABFILE)" ]; then
   sudo printf "\n//$IP/backups  /mnt/backup/  cifs  user=$USERNAME,password=$PASSWORD,vers=1.0,_netdev,x-systemd.automount  0  0" | sudo tee -a "$FSTABFILE" >/dev/null
   echo "Mount added "
-  sudo mount -o remount -a
+  sudo mount -av
 fi
 
 # FURTHER SETUP
